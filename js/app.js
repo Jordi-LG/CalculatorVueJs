21b997c6d219f5bed9d9143bfae294ffdbe3c2a1
Jordi-LG <jordy.leguet@gmail.com>
2020-06-07 17:27:51 +0200

    View done + collect input on button click

--- js/app.js
+++ js/app.js
@@ -1,11 +1,21 @@
 let vm = new Vue ({
-  el: ".container",
+  el: "#app",
   data: {
+    total: 0,
+    current: 0,
+    operator: '',
     rowNumber: [
       [7, 8, 9, '+'],
       [4, 5, 6, '-'],
       [1, 2, 3, '*'],
       [0, 'C', '=', '/']
     ]
+  },
+
+  methods: {
+    select: function(input) {
+      console.log(typeof input);
+      
+    }
   }
 })
